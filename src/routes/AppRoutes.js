b549f9580cf5c8842258f09eb20b84a255b55e76
Sara Duque <98819311+luciaduque248@users.noreply.github.com>
2026-09-11 17:10:14 -0500

    Añade ruta del panel de huésped

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-
-import FormHab from '../Admin/FormHab'
-import ListHabs from '../Admin/ListHabs'
-import Inicio from '../pages/Inicio'
-import Login from '../pages/Login'
-import Perfil from '../Admin/Dashboard'
+import FormHab from '../Admin/FormHab';
+import ListHabs from '../Admin/ListHabs';
+import Inicio from '../pages/Inicio';
+import Login from '../pages/Login';
+import Perfil from '../Admin/Dashboard';
+import UserDashboard from '../pages/UserDashboard';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Inicio/>} ></Route>
-
-            <Route path='/list-habitaciones' element={<ListHabs/>} ></Route>
-            <Route path='/form-habitaciones' element={<FormHab/>} ></Route>
-            <Route path='/login' element={<Login/>} ></Route>
-            <Route path='/perfil' element={<Perfil/>} ></Route>
-
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Inicio />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/mi-cuenta' element={<UserDashboard />} />
+        <Route path='/perfil' element={<Perfil />} />
+        <Route path='/list-habitaciones' element={<ListHabs />} />
+        <Route path='/form-habitaciones' element={<FormHab />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default AppRoutes
+export default AppRoutes;
